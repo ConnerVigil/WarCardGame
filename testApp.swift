33a8bfaa03412f795eb51d53f016ef727e07414a
Conner Vigil <73132308+ConnerVigil@users.noreply.github.com>
2021-12-28 16:09:36 -0700

    Add files via upload

---
+++ testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Conner Vigil on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
